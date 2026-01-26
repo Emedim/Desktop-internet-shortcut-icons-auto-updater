d8057ffb7eefbdbe034d70f5d0a696ff6419855e
Andery <andqwey@gmail.com>
2026-01-26 03:18:05 +0300

    Первоначальная настройка проекта

---
+++ .vscode/build.bat
@@ -0,0 +1,25 @@
+@echo off
+cd /d "%~dp0\.."
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+
+cl src\main.c ^
+   /Zi ^
+   /Od ^
+   /W4 ^
+   /Fe:bin\app.exe ^
+   /Fo:build\ ^
+   /Fd:build\app.pdb ^
+   /link ^
+   /PDB:build\app_link.pdb ^
+   /INCREMENTAL:NO^
+   user32.lib
+
+rem /Zi     – включает отладочную информацию
+rem /Od     – выключает оптимизации
+rem /W4     – высокий уровень вспомогательных предупреждений
+rem /Fe:    – путь к .exe
+rem /Fo:    – путь к объектным файлам
+rem /Fd:    – куда компилятор кладёт .pdb – база данных с отладочной информацией
+rem /link   – после идут флаги только для линковщика
+rem /PDB:   – куда компоновщик складывает свой .pdb
+rem /INCREMENTAL: – отключает появление .ilk файла, ускоряющего пересборку проекта.
