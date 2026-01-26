eeb4d4f4747e1e387120c34466ebe593432f2e3d
Andery <andqwey@gmail.com>
2026-01-26 07:59:31 +0300

    Получение пути к рабочему столу

--- src/main.c
+++ src/main.c
@@ -1,7 +1,25 @@
+#include <windows.h>
+#include <shlobj.h>     //SHGetKnownFolderPath
 #include <stdio.h>
 
-int main()
+int main(void)
 {
-    printf("hellow world!\n");
+    PWSTR desktopPath = NULL;
+    HRESULT desktopPathResult = SHGetKnownFolderPath
+    (
+        &FOLDERID_Desktop,
+        0,
+        NULL,
+        &desktopPath
+    );
+
+    if (FAILED(desktopPathResult))
+    {
+        printf("SHGetKnownFolderPath failed, HRESULT = 0x%08X\n", (unsigned)desktopPathResult);
+        return 1;
+    }
+    wprintf(L"Desktop path: %ls\n", desktopPath);
+    CoTaskMemFree(desktopPath);     //освобождение пемяти из com-кучи
+
     return 0;
 }
